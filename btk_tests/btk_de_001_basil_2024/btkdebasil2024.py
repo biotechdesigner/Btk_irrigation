b0e0525f7f9b4ebdb267d363102f35a9fe61029f
Victor Rivero <85038276+biotechdesigner@users.noreply.github.com>
2024-05-16 10:44:53 +0200

    Day 136 data
    weather and irrigation data uploaded of day 136

--- btk_tests/btk_de_001_basil_2024/btkdebasil2024.py
+++ btk_tests/btk_de_001_basil_2024/btkdebasil2024.py
@@ -80,13 +80,14 @@ def run():
     irr.addevent(2024, 133, 6.668, 1.00)
     irr.addevent(2024, 134, 6.542, 1.00)
     irr.addevent(2024, 135, 4.73, 1.00)
+    irr.addevent(2024, 136, 6.918, 1.00)
 
     irr.savefile(os.path.join(module_dir,'btkdebasil2024.irr'))
     irr.loadfile(os.path.join(module_dir,'btkdebasil2024.irr'))
    
 
     #Run the model
-    mdl = fao.Model('2024-102','2024-135', par, wth, irr=irr, aq_Ks=True,
+    mdl = fao.Model('2024-102','2024-136', par, wth, irr=irr, aq_Ks=True,
                     comment = 'btk 2024 basil trial')
     mdl.run()
     print(mdl)
